ee5637b6b978fb2d3edb681420a012139ade631f
Nylbert Victor <nyldrift@hotmail.com>
2023-01-19 15:26:24 -0300

    final answer

--- challenge-1.js
+++ challenge-1.js
@@ -1,19 +1,63 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'breakingRecords' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY scores as parameter.
+ */
+
 function breakingRecords(scores) {
-    let high = scores[0], cHigh=0, low = scores[0], cLow=0;
+    let high = scores[0], records=[0,0], low = scores[0];
     for(let i=1;i<scores.length;i++)
       {
         if(high<scores[i])
         {
           high=scores[i];
-          cHigh++;
+          records[0]++;
         }
         if(low>scores[i])
         {
           low=scores[i];
-          cLow++;
+          records[1]++;
         }
       }
-  return cHigh, cLow;
+  return records;
 
 }
-console.log(breakingRecords([10,5,20,20,4,5,2,25,1]));
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const scores = readLine().replace(/\s+$/g, '').split(' ').map(scoresTemp => parseInt(scoresTemp, 10));
+
+    const result = breakingRecords(scores);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
